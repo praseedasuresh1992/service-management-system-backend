e1b28efefc9985ce6559aebcfef3513699825f07
Praseeda <praseeeda@gmail.com>
2025-11-29 23:41:04 +0530

    connected with front end

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const categoryroutes=require("./routes/category_routes")
 const complaintroutes=require("./routes/complaints_routes")
 const provideravailabiltyroutes=require("./routes/provider_availability_routes")
 const corsOptions = {
-  origin: ['http://localhost:5173'], // must be exact frontend URL
+  origin: ['https://service-management-system-client.vercel.app','http://localhost:5173'], // must be exact frontend URL
   credentials: true, // allow cookies, headers, etc.
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
