113c422bbea6d3c7a132ade202298b46e97bc7a3
Praseeda <praseeeda@gmail.com>
2025-12-06 19:37:18 +0530

    provider model corrected

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
+ app.options("/*", cors(corsOptions));
 app.use(cookieParser());          // 🚀 NOW BACKEND CAN READ COOKIES
 app.use(express.json());
 
