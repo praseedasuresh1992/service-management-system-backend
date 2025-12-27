37318fbe6cda3cd7789b4f44167791ef6ba0f8ee
Praseeda <praseeeda@gmail.com>
2025-12-28 01:30:08 +0530

    payment controller updation

--- routes/payment_routes.js
+++ routes/payment_routes.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/payment_controller');
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 
 
-router.post(
-  "/create-payment-intent",
-  auth.authuser,
-  paymentController.createPaymentIntent
-);
+
 router.post("/create-checkout-session", paymentController.createCheckoutSession);
 
 
