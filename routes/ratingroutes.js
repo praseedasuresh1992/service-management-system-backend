d26fa1f57d7b0a2e751d0bfd4b5919af93b12016
Praseeda <praseeeda@gmail.com>
2026-01-03 22:12:35 +0530

    rating controller corrected, authorization added

--- routes/ratingroutes.js
+++ routes/ratingroutes.js
@@ -8,7 +8,7 @@ router.post("/createrating", auth.authuser,auth.authorizeRoles("user") ,ratingCo
 
 // ⭐ Provider profile ratings
 router.get(
-  "/provider/:provider_id",auth.authuser,
+  "/providerrating/:provider_id",auth.authuser,
   auth.authorizeRoles("provider"),
   ratingController.getRatingsByProvider
 );
